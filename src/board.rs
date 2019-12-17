955371f4292406ef55ebcf70d137031d41e8587a
rustiphyde <rustiphyde@gmail.com>
2019-12-16 23:41:52 -0600

    Initial Commit

---
+++ src/board.rs
@@ -0,0 +1,27 @@
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum PieceColor {
+    Orange,
+    Green,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct GamePiece {
+    pub color: PieceColor,
+    pub crowned: bool,
+}
+
+impl GamePiece {
+    pub fn new(color: PieceColor) -> GamePiece {
+        GamePiece {
+            color,
+            crowned: false,
+        }
+    }
+
+    pub fn crowned(p: GamePiece) -> GamePiece {
+        GamePiece {
+            color: p.color,
+            crowned: true,
+        }
+    }
+}
