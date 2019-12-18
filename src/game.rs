ee4c633882e645d047175f2832e9cfc1d6636662
rustiphyde <rustiphyde@gmail.com>
2019-12-17 20:12:56 -0600

    Add pieces to board

--- src/game.rs
+++ src/game.rs
@@ -10,3 +10,32 @@ pub struct MoveResult {
     pub mv: Move,
     pub crowned: bool,
 }
+
+impl GameEngine {
+    pub fn new() -> GamePiece {
+        let mut engine =  GameEngine {
+            board: [[None; 8]; 8],
+            current_turn: PieceColor::Black,
+            move_count: 0,
+        };
+        engine.initialize_pieces();
+        engine   
+    }
+    
+    pub fn initialize_pieces();
+        [1, 3, 5, 7, 0, 2, 4, 6, 1, 3, 5, 7]
+        .iter()
+        .zip([0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2].iter())
+        .map(|(a,b)| (*a as usize, b as usize))
+        .for_each(|(x, y)| {
+            self.board[x][y] = Some(GamePiece::new(PieceColor::Orange));
+        });
+
+        [0, 2, 4, 6, 1, 3, 5. 7, 0, 2, 4, 6]
+        .iter()
+        .zip([5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7].iter())
+        .map(|(a, b)| (*a as usize, *b as usize))
+        .for_each(|(x, y)| {
+            self.board[x][y] = Some(GamePiece::new(PieceColor::Green));
+        });
+}
